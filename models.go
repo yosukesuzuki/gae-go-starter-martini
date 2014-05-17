3ff6e44d1d7dc615f47e3f120688108375ab6ad8
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2014-05-18 07:30:21 +0900

    fix expression

--- models.go
+++ models.go
@@ -16,14 +16,15 @@ import (
 	//    "github.com/PuerkitoBio/goquery"
 )
 
+// AdminPage store content for general pages
 type AdminPage struct {
 	DisplayPage bool      `datastore:"displaypage" json:"displaypage"`
 	Title       string    `datastore:"title" json:"title"`
-	Url         string    `datastore:"url" json:"url"`
+	URL         string    `datastore:"url" json:"url"`
 	PageOrder   int       `datastore:"pageorder" json:"pageorder"`
 	Content     string    `datastore:"content,noindex" json:"content"`
 	Images      string    `datastore:"images,noindex" json:"images"`
-	ExternalUrl string    `datastore:"externalurl" json:"externalurl"`
+	ExternalURL string    `datastore:"externalurl" json:"externalurl"`
 	Update      time.Time `datastore:"update" json:"update"`
 	Create      time.Time `datastore:"created" json:"created"`
 }
